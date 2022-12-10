d561a8780036664c7b67fbedeba920a2a125d03b
abclili123 <lilianaseoror@gmail.com>
2022-12-10 07:37:38 -0500

    added simple player scipt and placed player in first sample Scene

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float playerSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 playerDirection;
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float directionY = Input.GetAxisRaw("Vertical");
+        float directionX = Input.GetAxisRaw("Horizontal");
+        playerDirection = new Vector2(directionX, directionY).normalized;
+
+        if(directionX<0){
+            this.transform.localScale = new Vector3(-0.12f,0.12f,0.12f);
+        }
+        else if(directionX>0){
+            this.transform.localScale = new Vector3(0.12f,0.12f,0.12f);
+        }
+
+        if( Mathf.Abs(directionX)*playerSpeed > 0 || Mathf.Abs(directionY)*playerSpeed > 0){
+            anim.SetBool("speed", true);
+        }
+        else{
+            anim.SetBool("speed", false);
+        }
+    
+    }
+
+    void FixedUpdate(){
+        rb.velocity = new Vector2(playerDirection.x * playerSpeed, playerDirection.y * playerSpeed);
+    }
+}
