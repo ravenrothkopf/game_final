c6e2b9b5c4d1bcdddd5ff396aca911c89a7ff821
fconyechere <91437562+fconyechere@users.noreply.github.com>
2022-12-11 22:17:57 -0500

    i keep doing this wrong

---
+++ Assets/Scripts/Player 2.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // moving player
+    public float playerSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 playerDirection;
+
+    // animation contorller
+    public Animator anim;
+    public Animator enemyAnim;
+
+    // health
+    public int maxHealth;
+    int curHealth;
+    public bool isAlive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        curHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float directionY = Input.GetAxisRaw("Vertical");
+        float directionX = Input.GetAxisRaw("Horizontal");
+        playerDirection = new Vector2(directionX, directionY).normalized;
+
+        if(directionX<0){
+            this.transform.localScale = new Vector3(-0.12f,0.12f,0.12f);
+        }
+        else if(directionX>0){
+            this.transform.localScale = new Vector3(0.12f,0.12f,0.12f);
+        }
+
+        if( Mathf.Abs(directionX)*playerSpeed > 0 || Mathf.Abs(directionY)*playerSpeed > 0){
+            anim.SetBool("speed", true);
+        }
+        else{
+            anim.SetBool("speed", false);
+        }
+    
+    }
+
+    void FixedUpdate(){
+        rb.velocity = new Vector2(playerDirection.x * playerSpeed, playerDirection.y * playerSpeed);
+    }
+
+        public void TakeDamage(int damage){
+        curHealth -= damage;
+        anim.SetTrigger("hurt");
+        
+        // play animation
+
+        if(curHealth <=0){
+            Die();
+        }
+
+    }
+
+    void Die(){
+        // die animation
+        isAlive = false;
+        rb.constraints = RigidbodyConstraints2D.FreezePosition;
+        anim.SetBool("isDead", true);
+        this.enabled = false;
+    }
+}
